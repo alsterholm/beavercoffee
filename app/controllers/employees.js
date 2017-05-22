890090f403c0b20a50da3e46d457f4f9819a4834
JimmyLindstrom <jimmylindstrom79@gmail.com>
2017-05-22 22:23:13 +0200

    added functions for employee endpoint

--- app/controllers/employees.js
+++ app/controllers/employees.js
@@ -1,15 +1,29 @@
+const Employee = require ('../models/employee');
+
 const EmployeesController = {
   index (req, res) {
-
+  	//list all employees
+  	Employee.find({}, function(err, employees) {
+  		if (err)
+  			res.send(err);
+  		res.json(employees);
+  	});
   },
 
   store (req, res) {
-
+  	// create new employee
+  	const new_employee = new Employee(req.body);
+  	new_employee.save(function(err, employee) {
+  		if(err)
+  			res.send(err);
+  		res.json(employee);
+  	});
   },
 
   update (req, res) {
-
+  	// update employee
   },
+
 };
 
 module.exports = EmployeesController;
