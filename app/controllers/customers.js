b8b3da79786d89a92bbc4f9334344840bc75bdd9
JimmyLindstrom <jimmylindstrom79@gmail.com>
2017-05-22 22:22:52 +0200

    added functions for customer endpoint

--- app/controllers/customers.js
+++ app/controllers/customers.js
@@ -3,7 +3,7 @@ const Customer = require('../models/customer');
 const CustomersController = {
   index (req, res) {
     // List all customers
-    const filter = req.query.filter;
+    const filter = req.query.filter; // todo fix filter!!
     Customer.find({}, function (err, customers){
       if (err)
         res.send(err);
@@ -13,21 +13,33 @@ const CustomersController = {
 
   show (req, res) {
     // Show details about a given customer
-  },
-
-  store (req, res) {
-    const customer = new Customer(req.body);
-    customer.save(function(err, customer) {
+    Customer.find({_id: req.params.id}, function (err, customer){
       if (err)
         res.send(err);
       res.json(customer);
     });
+
+  },
+
+  store (req, res) {
     // Register a new customer
+    const new_customer = new Customer(req.body);
+    new_customer.save(function(err, customer) {
+      if (err)
+        res.send(err);
+      res.json(customer);
+    });
   },
 
   update (req, res) {
-
+    //updates a customer
+    Customer.findOneAndUpdate({_id: req.params.id}, req.body, function(err, customer) {
+      if (err)
+        res.send(err);
+      res.json(customer);
+    });
   },
+
 };
 
 module.exports = CustomersController;
