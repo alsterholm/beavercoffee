bc1af7f0f2d37f8d705ae505ea270daa58e5943b
JimmyMaksymiw <jimmy.maksymiw@gmail.com>
2017-05-24 00:48:02 +0200

    added employee comment

--- app/controllers/employeeComments.js
+++ app/controllers/employeeComments.js
@@ -1,6 +1,18 @@
+const Employee = require ('../models/employee');
+
 const EmployeeCommentsController = {
   store (req, res) {
+    const comment = {
+      _authorId: req.body._authorId,
+      body: req.body.text,
+      date: new Date()
+    };
 
+    Employee.findOneAndUpdate({_id: req.params.id}, {$push: {comments: comment}}, {new: true},  function(err, customer) {
+      if (err)
+        res.send(err);
+      res.json(customer);
+    });
   },
 };
 
