c27a059e0278f5ff235f4fd1d7274c58766293a4
Andreas Indal <andreas@rocketship.se>
2017-05-23 11:34:15 +0200

    wip

--- app/controllers/orders.js
+++ app/controllers/orders.js
@@ -11,6 +11,18 @@ const OrdersController = {
   },
 
   store (req, res) {
+    const products = req.body.products // ["ajwndjkakdniada2kawd", "andwkankjdnakuwdn"]
+
+    Produt
+
+    Coffeeshop.findOne({_id: req.params.id}, (err, coffeeshop) => {
+      coffeshop.products.each(p => {
+        Products.update({ _id: p._id }, {
+          $dec: quantity
+        })
+      })
+    })
+
     Order.create(req.body, (err, model) => {
       if (err) console.log(err);
 
@@ -29,7 +41,7 @@ const OrdersController = {
 
   delete (req, res) {
     // Delete a given order
-    
+
   },
 };
 
